22230d8d892db7a8c774f63b231ad9e99347a017
lesimoes <lesimoes0@gmail.com>
2020-08-03 21:38:07 -0300

    remove config dir

--- src/config/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const setupRoute = require('./route');
-const setupMiddlewares = require('./middlewares');
-
-const app = express();
-setupRoute(app);
-setupMiddlewares(app);
-
-module.exports = app;
